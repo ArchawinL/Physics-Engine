b3c5b810523005b187443b2123f7a0270bd52dd2
Archawork <a.leelapanang@gmail.com>
2024-06-24 19:52:27 +0700

    Implemented: - 2D movement

--- src/particle.py
+++ src/particle.py
@@ -1,10 +1,13 @@
 class Particle:
-    def __init__(self, mass, position, velocity):
+    def __init__(self, mass, position=(0, 0), velocity=(0, 0)):
         self.mass = mass
         self.position = position
         self.velocity = velocity
 
     def move(self):
-        self.position += self.velocity
+        self.position = tuple(map(sum, zip(self.position, self.velocity)))
+
+    def set_velocity(self, velocity):
+        self.velocity = velocity
 
 
