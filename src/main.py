245c0842a72ab68936e064bacc1f1ad5036c9941
Archawork <a.leelapanang@gmail.com>
2024-07-04 23:02:43 +0700

    Started working on utils.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ from src.objects import Particle
 
 
 def main():
-    ball = Particle(250, "ball", (800, 450), (5, 0), (0, 0.1))
+
+    ball1 = Particle(250, "ball1", (800, 450), (5, 0), (0, 0.1))
+    ball2 = Particle(250, "ball2", (810, 460), (-4, 0), (0, 0.1))
+    ball3 = Particle(250, "ball3", (760, 430), (-5, 0), (0, 0.1))
+    ball4 = Particle(250, "ball4", (1000, 430), (7, 0), (0, 0.1))
+
+
     mw = Window('Custom Title', 1600, 900)
 
-    mw.render([ball])
+    mw.render([ball1, ball2, ball3, ball4])
 
 
 if __name__ == '__main__':
---
+++ src/utils.py
@@ -0,0 +1,9 @@
+from src import *
+from src.objects import Particle
+
+
+def randomise_balls(num):
+    ball_list = []
+    for ball in range(num):
+        pass
+
