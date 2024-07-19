d1cd75d2e96e34a235983417b46f2136775fc0da
Archawork <a.leelapanang@gmail.com>
2024-07-19 12:56:33 +0700

    Added: - Collision response in the tangential plane.

--- src/objects.py
+++ src/objects.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Object:
 
-    def __init__(self, tag, position=np.array([0, 0]), r=50):
+    def __init__(self, tag, position=np.array([0, 0]), r=20):
         self.tag = tag
         self.s = position
         self.x = position[0]
@@ -13,7 +13,8 @@ class Object:
 
 
 class Particle(Object):
-    def __init__(self, mass, tag, position=np.array([0, 0]), velocity=np.array([0, 0]), acceleration=np.array([0, 0]), r=50,
+    def __init__(self, mass, tag, position=np.array([0, 0]), velocity=np.array([0, 0]), acceleration=np.array([0, 0]),
+                 r=50,
                  border=(1600, 900)):
         super().__init__(tag, position, r)
         self.mass = mass
@@ -42,11 +43,27 @@ class Particle(Object):
 
     def detect_collision(self, *particles):
         for p in particles:
-            disp = self.s - p.s
+            disp_v = self.s - p.s
 
-            if np.linalg.norm(disp) < self.r*2:
-                self.s += np.divide(disp, 2)
-                p.s -= np.divide(disp, 2)
+            if np.linalg.norm(disp_v) < self.r * 2 and self.tag != p.tag:
+                self.s += np.divide(disp_v, 2)
+                p.s -= np.divide(disp_v, 2)
+                disp_v = self.s - p.s
+
+                """Tangential Velocities calculation"""
+                n_hat = np.divide(disp_v, np.linalg.norm(disp_v))
+                t_hat = np.array([-n_hat[1], n_hat[0]], float)
+
+                v_1tp = np.multiply(t_hat, np.dot(t_hat, self.v))
+                v_2tp = np.multiply(t_hat, np.dot(t_hat, p.v))
+
+                """Normal Velocities calculation"""
+                v_1n = np.dot(n_hat, self.v)
+                v_2n = np.dot(n_hat, p.v)
+                """
+                self.set_velocity(v_1tp)
+                p.set_velocity(v_2tp)
+                """
 
     def set_velocity(self, v_tr: np.array):
         self.v = v_tr
@@ -58,4 +75,5 @@ class Particle(Object):
         return self.v[1]
 
     def draw(self):
-        dpg.draw_circle(center=(float(self.s[0]), float(self.s[1])), radius=self.r, color=(255, 255, 255), tag=self.tag, fill=(255, 255, 255))
+        dpg.draw_circle(center=(float(self.s[0]), float(self.s[1])), radius=self.r, color=(255, 255, 255), tag=self.tag,
+                        fill=(255, 255, 255))
